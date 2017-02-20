67db4da8fed8f0fc41ba4c1fd78936132d3ae3f9
fjohnson1991 <felicity.johnson.mail@gmail.com>
2017-02-19 21:20:08 -0500

    added viewModel

--- SolvePuzzle/SolvePuzzle/PuzzleViewController.swift
+++ SolvePuzzle/SolvePuzzle/PuzzleViewController.swift
@@ -16,39 +16,24 @@ class PuzzleViewController: UIViewController, UICollectionViewDelegateFlowLayout
     var itemSize: CGSize!
     var numberOfRows: CGFloat!
     var numberOfColumns: CGFloat!
-    var imageSlices = [UIImage]()
-    var isSolved = false
+    let puzzleViewModel = PuzzleViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         presentAlertWithTitle(title: "Let's Play!", message: "Solve the puzzle by holding down on a cell and dragging it to its correct position. Solving the puzzle unlocks the information on Posse's employees. Don't want to play? Click \"Skip\" below.")
         configLayout()
-        configCellLayout()
         configCollectionView()
+        configCellLayout()
     }
-    
-    lazy var originalImageOrderArray = [UIImage]()
-    lazy var programmerArray = [Programmer]()
     
     func presentAlertWithTitle(title: String, message : String) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let OKAction = UIAlertAction(title: "OK", style: .default) {(action: UIAlertAction) in
-            self.imageSlices.removeAll()
-            self.programmerArray.removeAll()
-            self.populateImages()
-            self.randomize()
-            self.populateProgrammerInfo()
-            self.checkCellCount()
-            self.isSolved = false
+            self.puzzleViewModel.tryToSolvePuzzle()
             self.imageCollectionView.reloadData()
         }
         let skipAction = UIAlertAction(title: "Skip", style: .default) {(action: UIAlertAction) in
-            self.isSolved = true
-            self.imageSlices.removeAll()
-            self.programmerArray.removeAll()
-            self.populateImages()
-            self.populateProgrammerInfo()
-            self.checkCellCount()
+            self.puzzleViewModel.skipPuzzle()
             self.imageCollectionView.reloadData()
             self.presentSuccessAlertWithTitle(title: "Next Step", message: "Click the cells below to find out about Posse's employers.")
         }
@@ -60,10 +45,18 @@ class PuzzleViewController: UIViewController, UICollectionViewDelegateFlowLayout
     func configLayout() {
         self.view.backgroundColor = UIColor.white
         self.title = "Posse Puzzle"
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Play Again", style: .done, target: self, action: #selector(playAgain))
-            self.navigationItem.rightBarButtonItem?.setTitleTextAttributes([NSFontAttributeName: UIFont.systemFont(ofSize: 12, weight: UIFontWeightSemibold), NSForegroundColorAttributeName: UIColor.white],for: UIControlState.normal)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Play Again", style: .done, target: self, action: #selector(playAgainButton))
+        self.navigationItem.rightBarButtonItem?.setTitleTextAttributes([NSFontAttributeName: UIFont.systemFont(ofSize: 12, weight: UIFontWeightSemibold), NSForegroundColorAttributeName: UIColor.white],for: UIControlState.normal)
     }
-
+    
+    func playAgainButton() {
+        puzzleViewModel.playAgain()
+        imageCollectionView.removeFromSuperview()
+        configCellLayout()
+        configCollectionView()
+        presentAlertWithTitle(title: "Let's Play!", message: "Solve the puzzle by holding down on a cell and dragging it to its correct position. Solving the puzzle unlocks the information on Posse's employees. Don't want to play? Click \"Skip\" below.")
+    }
+    
     func configCollectionView() {
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
         imageCollectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height), collectionViewLayout: layout)
@@ -93,70 +86,12 @@ class PuzzleViewController: UIViewController, UICollectionViewDelegateFlowLayout
         itemSize = CGSize(width: (screenWidth - totalWidthDeduction) / numberOfColumns, height: (screenHeight - totalHeightDeduction) / numberOfRows)
     }
     
-    // MARK: - Populate & configure data
-    func populateImages() {
-        for name in 1...8 {
-            if let image = UIImage(named:String(name)){
-                imageSlices.append(image)
-                originalImageOrderArray.append(image)
-            }
-        }
-    }
-    
-    func randomize() {
-        for num in 0..<8 {
-            let randomIndex = Int(arc4random_uniform(UInt32(imageSlices.count)))
-            if num != randomIndex {
-                swap(&imageSlices[num], &imageSlices[randomIndex])
-            }
-        }
-    }
-    
-    func populateProgrammerInfo() {
-        DataStore.getJson { (parsedJson) in
-            for location in parsedJson {
-                let location = Location(dict: location)
-                guard let services = location?.services else { print("Error unwrapping services"); return }
-                for service in services {
-                    guard let platform = service["platform"] as? String else { print("Error retrieving platform"); return }
-                    guard let programmers = service["programmers"] as? [[String: Any]] else { print("Error getting service[programmers]"); return }
-                    for prog in programmers {
-                        let newProg = Programmer(dict: prog)
-                        guard let unwrappedNewProg = newProg else { print("Error unwrapping programmer in populateProgrammerInfo"); return }
-                        unwrappedNewProg.location = location
-                        unwrappedNewProg.platform = platform
-                        self.programmerArray.append(unwrappedNewProg)
-                    }
-                }
-            }
-        }
-    }
-    
-    func checkCellCount() {
-        if programmerArray.count < imageSlices.count {
-            let newProg = Programmer(name: "", favorite_color: "", age: 0, weight: 0.0, phone: "", is_artist: false)
-            programmerArray.append(newProg)
-        }
-    }
-    
-    func playAgain() {
-        programmerArray.removeAll()
-        imageSlices.removeAll()
-        originalImageOrderArray.removeAll()
-        imageCollectionView.removeFromSuperview()
-        presentAlertWithTitle(title: "Let's Play!", message: "Solve the puzzle by holding down on a cell and dragging it to its correct position. Solving the puzzle unlocks the information on Posse's employees. Don't want to play? Click \"Skip\" below.")
-        configLayout()
-        configCellLayout()
-        configCollectionView()
-        isSolved = false
-    }
-    
     // MARK: - Handle changing order of images
     func handleLongGesture(gesture: UILongPressGestureRecognizer) {
         switch(gesture.state){
         case UIGestureRecognizerState.began:
             guard let selectedIndexPath = self.imageCollectionView.indexPathForItem(at: gesture.location(in: self.imageCollectionView)) else { return }
-            imageCollectionView!.beginInteractiveMovementForItem(at: selectedIndexPath)
+            imageCollectionView.beginInteractiveMovementForItem(at: selectedIndexPath)
         case UIGestureRecognizerState.changed:
             imageCollectionView.updateInteractiveMovementTargetPosition(gesture.location(in: gesture.view!))
         case UIGestureRecognizerState.ended:
@@ -168,11 +103,11 @@ class PuzzleViewController: UIViewController, UICollectionViewDelegateFlowLayout
     
     func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         self.imageCollectionView.performBatchUpdates({
-            let itemToMove = self.imageSlices.remove(at: sourceIndexPath.item)
-            self.imageSlices.insert(itemToMove, at: destinationIndexPath.item)
+            let itemToMove = self.puzzleViewModel.imageSlices.remove(at: sourceIndexPath.item)
+            self.puzzleViewModel.imageSlices.insert(itemToMove, at: destinationIndexPath.item)
         }, completion: { completed in
-            if self.originalImageOrderArray == self.imageSlices {
-                self.isSolved = true
+            if self.puzzleViewModel.originalImageOrderArray == self.puzzleViewModel.imageSlices {
+                self.puzzleViewModel.isSolved = true
                 self.presentSuccessAlertWithTitle(title: "Great Job!", message: "Click the cells below to find out about Posse's employers.")
             }
         })
@@ -187,7 +122,7 @@ class PuzzleViewController: UIViewController, UICollectionViewDelegateFlowLayout
     
     // MARK: - Handle flip animation
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if isSolved == true {
+        if puzzleViewModel.isSolved == true {
             let cell = collectionView.cellForItem(at: indexPath) as! MapCollectionViewCell
             cell.flipCardAnimation()
         }
@@ -201,13 +136,13 @@ extension PuzzleViewController {
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageSlices.count
+        return puzzleViewModel.imageSlices.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as! MapCollectionViewCell
-        cell.frontView.imageView.image = imageSlices[indexPath.row]
-        cell.backView.programmer = programmerArray[indexPath.row]
+        cell.frontView.imageView.image = puzzleViewModel.imageSlices[indexPath.row]
+        cell.backView.programmer = puzzleViewModel.programmerArray[indexPath.row]
         return cell
     }
     
---
+++ SolvePuzzle/SolvePuzzle/PuzzleViewModel.swift
@@ -0,0 +1,112 @@
+//
+//  PuzzleViewModel.swift
+//  SolvePuzzle
+//
+//  Created by Felicity Johnson on 2/19/17.
+//  Copyright © 2017 FJ. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PuzzleViewModel {
+    
+    var imageSlices: [UIImage]!
+    var originalImageOrderArray: [UIImage]!
+    var programmerArray: [Programmer]!
+    var isSolved = false
+    
+    init(imageSlices: [UIImage], originalImageOrderArray: [UIImage], programmerArray: [Programmer]) {
+        self.imageSlices = imageSlices
+        self.originalImageOrderArray = originalImageOrderArray
+        self.programmerArray = programmerArray
+    }
+    
+    convenience init() {
+        self.init(imageSlices: [], originalImageOrderArray: [], programmerArray: [])
+    }
+    
+    // MARK: - Populate & configure data
+    func populateImages() {
+        for name in 1...8 {
+            if let image = UIImage(named:String(name)){
+                imageSlices.append(image)
+                originalImageOrderArray.append(image)
+            }
+        }
+    }
+    
+    func randomize() {
+        for num in 0..<8 {
+            let randomIndex = Int(arc4random_uniform(UInt32(imageSlices.count)))
+            if num != randomIndex {
+                swap(&imageSlices[num], &imageSlices[randomIndex])
+            }
+        }
+    }
+    
+    func checkCellCount() {
+        if programmerArray.count < imageSlices.count {
+            let newProg = Programmer(name: "", favorite_color: "", age: 0, weight: 0.0, phone: "", is_artist: false)
+            programmerArray.append(newProg)
+        }
+    }
+    
+    func getJson(with completion: @escaping ([[String: Any]]) -> Void) {
+        guard let filePath = Bundle.main.path(forResource: "ios_model_challenge", ofType: "json") else { print("error unwrapping json playgrounds file path"); return }
+        do {
+            let data = try? NSData(contentsOfFile: filePath, options: .uncached)
+            guard let castedData = data as? Data else { print("Error casting data as Data"); return }
+            guard let rawDictionary = try JSONSerialization.jsonObject(with: castedData, options: []) as? [String : Any] else { print("Error serializing JSON"); return }
+            guard let response = rawDictionary["response"] as? [String: Any] else { print("Error retrieving rawDictionary[response]"); return }
+            guard let parsedJSON = response["locations"] as? [[String: Any]] else { print("Error retrieving response[locations]"); return }
+            completion(parsedJSON)
+        } catch {}
+    }
+    
+    func populateProgrammerInfo() {
+        getJson { (parsedJson) in
+            for location in parsedJson {
+                let location = Location(dict: location)
+                guard let services = location?.services else { print("Error unwrapping services"); return }
+                for service in services {
+                    guard let platform = service["platform"] as? String else { print("Error retrieving platform"); return }
+                    guard let programmers = service["programmers"] as? [[String: Any]] else { print("Error getting service[programmers]"); return }
+                    for prog in programmers {
+                        let newProg = Programmer(dict: prog)
+                        guard let unwrappedNewProg = newProg else { print("Error unwrapping programmer in populateProgrammerInfo"); return }
+                        unwrappedNewProg.location = location
+                        unwrappedNewProg.platform = platform
+                        self.programmerArray.append(unwrappedNewProg)
+                    }
+                }
+            }
+        }
+    }
+    
+    func tryToSolvePuzzle() {
+        imageSlices.removeAll()
+        programmerArray.removeAll()
+        populateImages()
+        randomize()
+        populateProgrammerInfo()
+        checkCellCount()
+        isSolved = false
+    }
+    
+    func skipPuzzle() {
+        isSolved = true
+        imageSlices.removeAll()
+        programmerArray.removeAll()
+        populateImages()
+        populateProgrammerInfo()
+        checkCellCount()
+    }
+    
+    func playAgain() {
+        imageSlices.removeAll()
+        originalImageOrderArray.removeAll()
+        programmerArray.removeAll()
+        isSolved = false
+    }
+}
